7fa5baa02a00c7fe2d020e17bc1d52c0691fdb64
Oleg Nesterov <olnester@redhat.com>
2019-02-12 21:19:54 +0200

    added verification for input values

--- app/main/app/exercises/oil.go
+++ app/main/app/exercises/oil.go
@@ -16,65 +16,52 @@ func setupParams(fuelСonsumption, pricePerOneLiter float64, distance int) float
 	oneLitersForKilometers := 100 / fuelСonsumption
 	totalLiters := distanceInt / oneLitersForKilometers
 
-	//q := fmt.Sprintf("Quantity liters for 1 km: %.2f%s", consumptionOfLitersPerOneKilometer, "l")
-	//q := fmt.Sprintf("Количество литров на 1 км: %.2f%s", consumptionOfLitersPerOneKilometer, "л")
-	//t := fmt.Sprintf("Total liters for %.d kilometers: %.1f%s", distance, totalLiters, "l")
 	fmt.Println("\nРезультат:")
 	t := fmt.Sprintf("Количество литров на %.d километров: %.1f%s", distance, totalLiters, "л")
-	//fmt.Println(q)
 	fmt.Println(t)
 
 	return totalCosts
 }
 
-/*
-	consolescanner.Scan()
-
-	switch input := consolescanner.Text(); input {
-	case "darwin":
-		fmt.Println("OS X.")
-	case "linux":
-		fmt.Println("Linux.")
-	default:
-*/
+const s = "Неверный воод! Введите число > 0"
 
 func main() {
 	consolescanner := bufio.NewScanner(os.Stdin)
-	//fmt.Println("Welcome to the \"Calculating the cost of the trip\" program")
 	fmt.Println("Добро пожаловать в программу \"Расчет стоимости поездки\"\n")
-	// by default, bufio.Scanner scans newline-separated lines
 
 	for {
-		fmt.Print("Введите дистанцию в км : ")
-		consolescanner.Scan()
-		inputDistance := consolescanner.Text()
 
-		fmt.Print("Введите расход топлива на 100 км : ")
-		consolescanner.Scan()
-		inputfuelСonsumption := consolescanner.Text()
+		distance := inputDestantion(consolescanner)
+		for distance <= 0 {
+			fmt.Println(s)
+			distance = inputDestantion(consolescanner)
+		}
 
-		fmt.Print("Введите стоимость 1-ого литра в гривнах : ")
-		consolescanner.Scan()
-		inputpricePerOneLiter := consolescanner.Text()
+		fuelСonsumption := fuelConsumption(consolescanner)
+		for fuelСonsumption <= 0 {
+			fmt.Println(s)
+			fuelСonsumption = fuelConsumption(consolescanner)
+
+		}
+		pricePerOneLiter := price(consolescanner)
+		for pricePerOneLiter <= 0 {
+			fmt.Println(s)
+			pricePerOneLiter = price(consolescanner)
+		}
 
 		if err := consolescanner.Err(); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		distance, _ := strconv.Atoi(inputDistance)
-		fuelСonsumption, _ := strconv.ParseFloat(inputfuelСonsumption, 64)
-		pricePerOneLiter, _ := strconv.ParseFloat(inputpricePerOneLiter, 64)
 		result := setupParams(fuelСonsumption, pricePerOneLiter, distance)
-		//tc := fmt.Sprintf("Total costs for %.d kilometers: %.2fuah ", distance, result)
 		tc := fmt.Sprintf("Стоимость топлива на %.d километров: %.2fгрн. ", distance, result)
 		fmt.Println(tc)
 
 		for {
 			fmt.Print("\nПродолжить? (y/n или да/нет): ")
 			consolescanner.Scan()
-			nextStep := consolescanner.Text()
-			nextStep = strings.ToLower(nextStep)
+			nextStep := strings.ToLower(consolescanner.Text())
 
 			if nextStep == "y" || nextStep == "да" {
 				break
@@ -86,4 +73,25 @@ func main() {
 			}
 		}
 	}
+}
+
+func price(consolescanner *bufio.Scanner) float64 {
+	fmt.Print("Введите стоимость 1-ого литра в гривнах : ")
+	consolescanner.Scan()
+	pricePerOneLiter, _ := strconv.ParseFloat(consolescanner.Text(), 64)
+	return pricePerOneLiter
+}
+
+func fuelConsumption(consolescanner *bufio.Scanner) float64 {
+	fmt.Print("Введите расход топлива на 100 км : ")
+	consolescanner.Scan()
+	fuelСonsumption, _ := strconv.ParseFloat(consolescanner.Text(), 64)
+	return fuelСonsumption
+}
+
+func inputDestantion(consolescanner *bufio.Scanner) int {
+	fmt.Print("\nВведите дистанцию в км : ")
+	consolescanner.Scan()
+	distance, _ := strconv.Atoi(consolescanner.Text())
+	return distance
 }
