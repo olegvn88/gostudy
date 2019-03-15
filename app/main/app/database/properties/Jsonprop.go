5515eb5fe5ba6c9beecdd0c0e7deaefe3bb5dbd0
Oleg Nesterov <olnester@redhat.com>
2019-03-15 20:31:02 +0200

    create json config

--- app/main/app/database/properties/Jsonprop.go
+++ app/main/app/database/properties/Jsonprop.go
@@ -17,7 +17,7 @@ type jsonData struct {
 }
 
 func JsonProp() jsonData {
-	jData, err := ioutil.ReadFile("/home/onest/go/src/github.com/olegvn88/gostudy/app/main/app/database/properties/properties.json")
+	jData, err := ioutil.ReadFile("/home/onest/go/src/github.com/olegvn88/gostudy/app/main/app/database/properties/config/properties.json")
 	if err != nil {
 		panic(err)
 	}
